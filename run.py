c39fbf6daae11d439dec80e7e1378eb622358cd3
lul_prof <israelmutua539@gmail.com>
2025-05-15 10:18:16 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
+        # Initialize database tables
+    with app.app_context():
+        db.create_all()
+        
     app.run()
